101ae268fae9fd5f29ee8bb4307516389f6fe789
Rohuu <rohuu99022@gmail.com>
2023-02-11 10:48:42 +0530

    established connection

---
+++ src/com/company/DbFunctions.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class DbFunctions {
+    public Connection connectToDb(String dbname, String user, String pass) {
+        Connection conn = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbname, user, pass);
+            if (conn != null) {
+                System.out.println("Connection Established");
+            } else {
+                System.out.println("Connection Failed");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return conn;
+    }
+}
