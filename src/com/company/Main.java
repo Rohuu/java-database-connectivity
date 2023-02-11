fbe19921c01fbfcf3e27ca9413f47288903e658c
Rohuu <rohuu99022@gmail.com>
2023-02-11 10:46:09 +0530

    established connection

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.sql.Connection;
+
+public class Main {
+
+    public static void main(String[] args) {
+	DbFunctions db=new DbFunctions();
+    Connection conn=db.connectToDb("db1","postgres","postgres");
+    }
+}
